56c8a02d11e828f547a8582129b9688bf6c1b6bc
Kuba-Brozek <kubabrozek9@gmail.com>
2021-12-03 18:05:24 +0100

    FINISHED !!!!!

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,34 @@
 package com.company
 
 import java.util.*
+import kotlin.collections.ArrayList
 
-    private val ChromosomeList: MutableList<Chromosome> = mutableListOf()
-    private val ChromosomeList2: MutableList<Chromosome> = mutableListOf()
-    private val VWList: MutableList<VW> = mutableListOf()
-    private val PercentageRangeList: MutableList<PercentageRangeClass> = mutableListOf()
+
+private val ChromosomeList = ArrayList<Chromosome>()
+    private val ChromosomeList2 = ArrayList<Chromosome>()
+    private val VWList = ArrayList<VW>()
+    private val PercentageRangeList = ArrayList<PercentageRangeClass>()
     fun main() {
         val scanner = Scanner(System.`in`)
         println("Podaj liczbe chromosomów: ")
         val ChromosomesNumber = scanner.nextInt()
-        /*
-        System.out.println("Podaj wagę dla każdego bitu");
-        for(int i = 0; i< 10; i++){
-            int Weight = scanner.nextInt();
-            WeightList.add(i, Weight);
-            WeightListCalculated.add(i, 0);
-        }
-        System.out.println("Podaj wartość dla każdego bitu");
-        for(int i = 0; i< 10; i++){
-            int Value = scanner.nextInt();
-            ValueList.add(i, Value);
-            ValueListCalculated.add(i, 0);
-        }*/println("Podaj Pk i Pm jako wartość od 0 do 1 w typie double: ")
+        println("Podaj Pk i Pm jako wartość od 0 do 1 w typie double: ")
         val Pk = scanner.nextDouble()
         val Pm = scanner.nextDouble()
         println("Podaj maksymalną wagę plecaka: ")
         val MaxWeight = scanner.nextInt()
-        println("Podaj liczbę maksymalnych wystąpień wartości funkcji przystosowania")
-        val NumOfMaxFitFunOccur = scanner.nextInt()
-        var NumOfFitListIterations = 0
-        var NumOfIterations = 0
+        println("Podaj liczbę maksymalnych wystąpień wartości funkcji przystosowania: ")
+        val numOfMaxFitFunOccur = scanner.nextInt()
+        var numOfFitListIterations = 0
+        var numOfIterations = 0
 
         /*for(int i = 0; i< 10; i++){
         System.out.println("Podaj Wagę i Wartość dla " + i + " bitu: ");
             int Weight = scanner.nextInt();
             int Value = scanner.nextInt();
             VWList.add(new VW(Weight, Value));
-        }
-        */VWList.add(VW(4, 12))
+        }*/
+        VWList.add(VW(4, 12))
         VWList.add(VW(9, 6))
         VWList.add(VW(12, 10))
         VWList.add(VW(4, 3))
@@ -50,46 +40,48 @@ import java.util.*
         VWList.add(VW(8, 6))
         println(" | Początkowa pula chromosomów | ")
         for (i in 0 until ChromosomesNumber) {
-            val RandNumHelper = RandomNumberChromosome()
-            ChromosomeList.add(Chromosome(RandNumHelper, 0.0))
-            ChromosomeList2.add(Chromosome(RandNumHelper, 0.0))
+            val randNumHelper = RandomNumberChromosome()
+            ChromosomeList.add(Chromosome(randNumHelper, 0.0))
+            ChromosomeList2.add(Chromosome(randNumHelper, 0.0))
             PercentageRangeList.add(PercentageRangeClass(0.0, 0.0))
             System.out.print(" |  " + ChromosomeList[i].ch.toString() + " | ")
         }
         for (i in 0 until ChromosomesNumber) {
-            val BinarValueBuilder = StringBuilder(Integer.toBinaryString(ChromosomeList[i].ch))
-            while (BinarValueBuilder.length < 10) {
-                BinarValueBuilder.insert(0, "0")
+            var binarValue: String = Integer.toBinaryString(ChromosomeList[i].ch)
+            while (binarValue.length < 10) {
+                binarValue = "0$binarValue"
             }
-            val BinarValue = BinarValueBuilder.toString()
-            ChromosomeList[i].chromosomeBinar = BinarValue
-            ChromosomeList2[i].chromosomeBinar = BinarValue
+
+            ChromosomeList[i].chromosomeBinar = binarValue
+            ChromosomeList2[i].chromosomeBinar = binarValue
         }
-        var Final = 0
-        val FinalValue = 0
+        var final = 0
         while (true) {
-            var ValueOfFitFunForAllChromosomes = 0
-            var PercentageIterationValue = 0.00
-            var aValuePercent = 0.0
-            val ValueOfFitFun = 0
+            var valueOfFitFunForAllChromosomes: Int = 0
+            var percentageIterationValue: Double = 0.00
+            var aValuePercent: Double = 0.0
             for (j in 0 until ChromosomesNumber) {
                 //that loop calculates Weight of each chromosome based on users keyboard input, checks if weight is bigger than admissible, and if so it throws one of bits from 1 to 0 untill
                 // it finds first weight which is below limit
-                var WeightOfFitFun = 0
-                var CalculatedWeight = 0
+                var weightOfFitFun = 0
+                var calculatedWeight = 0
                 for (i in 0..9) {
-                    val Check: String = ChromosomeList[j].chromosomeBinar!!.substring(i, i+1)
-                    CalculatedWeight = Check.toInt() * VWList[i].weight
-                    WeightOfFitFun += CalculatedWeight
+                    val Check: Char = ChromosomeList[j].chromosomeBinar!![i]
+                    calculatedWeight = Integer.parseInt(Check.toString()) * VWList[i].weight
+                    weightOfFitFun += calculatedWeight
                 }
-                ChromosomeList[j].weightOfChromosome = WeightOfFitFun
+                ChromosomeList[j].weightOfChromosome = weightOfFitFun
                 if (MaxWeight < ChromosomeList[j].weightOfChromosome) {
+                    var xd = 0
                     while (true) {
+                        xd++
                         val Bit = RandomNumberLokusPm()
                         val Beg: String = ChromosomeList[j].chromosomeBinar!!.substring(0, Bit - 1)
                         val End: String = ChromosomeList[j].chromosomeBinar!!.substring(Bit)
                         val Mutate: String = ChromosomeList[j].chromosomeBinar!!.substring(Bit-1, Bit)
-                        if (Mutate == "1") {
+
+                        if (Mutate.equals("1")) {
+
                             val ChromosomeBinarFinal = Beg + "0" + End
                             ChromosomeList[j].chromosomeBinar = ChromosomeBinarFinal
                             ChromosomeList2[j].chromosomeBinar = ChromosomeBinarFinal
@@ -106,32 +98,32 @@ import java.util.*
                 }
             }
             for (j in 0 until ChromosomesNumber) {
-                var ValueofFitFun = 0
-                var CalculatedValue = 0
+                var valueofFitFun = 0
+                var calculatedValue = 0
                 for (i in 0..9) {
-                    val Checker: String = ChromosomeList[j].chromosomeBinar!!.substring(i, i+1)
-                    CalculatedValue = Checker.toInt() * VWList[i].value
-                    ValueofFitFun += CalculatedValue
+                    val checker: Char = ChromosomeList[j].chromosomeBinar!![i]
+                    calculatedValue = Integer.parseInt(checker.toString()) * VWList[i].value
+                    valueofFitFun += calculatedValue
                 }
-                ChromosomeList[j].valueOfChromosome = ValueOfFitFun
-                ValueOfFitFunForAllChromosomes += ChromosomeList[j].valueOfChromosome
+                ChromosomeList[j].valueOfChromosome = valueofFitFun
+                valueOfFitFunForAllChromosomes += ChromosomeList[j].valueOfChromosome
             }
-            if (ValueOfFitFunForAllChromosomes > Final) {
-                Final = ValueOfFitFunForAllChromosomes
-                NumOfFitListIterations = 1
-            } else if (ValueOfFitFunForAllChromosomes == Final) {
-                NumOfFitListIterations++
+            if (valueOfFitFunForAllChromosomes > final) {
+                final = valueOfFitFunForAllChromosomes
+                numOfFitListIterations = 1
+            } else if (valueOfFitFunForAllChromosomes == final) {
+                numOfFitListIterations++
             }
-            if (NumOfFitListIterations == NumOfMaxFitFunOccur) {
+            if (numOfFitListIterations == numOfMaxFitFunOccur) {
                 break
             }
             for (i in 0 until ChromosomesNumber) {
-                ChromosomeList[i].percent = PercentageValue(ChromosomeList[i].valueOfChromosome, ValueOfFitFunForAllChromosomes)
+                ChromosomeList[i].percent = PercentageValue(ChromosomeList[i].valueOfChromosome, valueOfFitFunForAllChromosomes)
             }
             for (i in 0 until ChromosomesNumber) {
-                aValuePercent = PercentageIterationValue
-                PercentageIterationValue += ChromosomeList[i].percent
-                PercentageRangeList[i] = PercentageRangeClass(aValuePercent, PercentageIterationValue)
+                aValuePercent = percentageIterationValue
+                percentageIterationValue += ChromosomeList[i].percent
+                PercentageRangeList[i] = PercentageRangeClass(aValuePercent, percentageIterationValue)
             }
             for (i in 0 until ChromosomesNumber) {
                 val x = RandomPercentageNumber()
@@ -143,11 +135,10 @@ import java.util.*
             }
             for (i in 0 until ChromosomesNumber) {
                 ChromosomeList2[i].ch = ChromosomeList[i].ch
-                val BinarValueBuilder = StringBuilder(Integer.toBinaryString(ChromosomeList[i].ch))
-                while (BinarValueBuilder.length < 10) {
-                    BinarValueBuilder.insert(0, "0")
+                var BinarValue: String = Integer.toBinaryString(ChromosomeList[i].ch)
+                while (BinarValue.length < 10) {
+                    BinarValue = "0$BinarValue"
                 }
-                val BinarValue = BinarValueBuilder.toString()
                 ChromosomeList[i].chromosomeBinar = BinarValue
                 ChromosomeList2[i].chromosomeBinar = BinarValue
             }
@@ -159,10 +150,10 @@ import java.util.*
                 PkhelperNumber += 2
                 val Lokus = RandomNumberLokusPk()
                 if (x < Pk) {
-                    val BegOneOf2: String = ChromosomeList2[random16 - 1].chromosomeBinar!!.substring(0, Lokus)
-                    val BegTwoOf2: String = ChromosomeList2[random162 - 1].chromosomeBinar!!.substring(0, Lokus)
-                    val EndOneOf2: String = ChromosomeList2[random16 - 1].chromosomeBinar!!.substring(Lokus, ChromosomeList[i].chromosomeBinar!!.length)
-                    val EndTwoOf2: String = ChromosomeList2[random162 - 1].chromosomeBinar!!.substring(Lokus, ChromosomeList[i].chromosomeBinar!!.length)
+                    val BegOneOf2: String = ChromosomeList2[random16-1].chromosomeBinar!!.substring(0, Lokus)
+                    val BegTwoOf2: String = ChromosomeList2[random162-1].chromosomeBinar!!.substring(0, Lokus)
+                    val EndOneOf2: String = ChromosomeList2[random16-1].chromosomeBinar!!.substring(Lokus, ChromosomeList[i].chromosomeBinar!!.length)
+                    val EndTwoOf2: String = ChromosomeList2[random162-1].chromosomeBinar!!.substring(Lokus, ChromosomeList[i].chromosomeBinar!!.length)
                     val One = BegOneOf2 + EndTwoOf2
                     val Two = BegTwoOf2 + EndOneOf2
                     ChromosomeList[PkhelperNumber-2].chromosomeBinar = One
@@ -173,14 +164,15 @@ import java.util.*
                 val x = RandomPercentageNumber()
                 val Lokus = RandomNumberLokusPm()
                 if (x < Pm) {
+
                     val Beg: String = ChromosomeList[i].chromosomeBinar!!.substring(0, Lokus - 1)
                     val End: String = ChromosomeList[i].chromosomeBinar!!.substring(Lokus)
-                    val Mutate: String = ChromosomeList[i].chromosomeBinar!!.substring(Lokus - 1, Lokus)
-                    if (Mutate == "0") {
+                    val Mutate: String = ChromosomeList[i].chromosomeBinar!!.substring(Lokus-1, Lokus)
+                    if (Mutate.equals("0")) {
                         val ChromosomeBinarFinal = Beg + "1" + End
                         ChromosomeList[i].chromosomeBinar = ChromosomeBinarFinal
-                    } else if (Mutate == "1") {
-                        val ChromosomeBinarFinal = Beg + "1" + End
+                    } else if (Mutate.equals("1")) {
+                        val ChromosomeBinarFinal = Beg + "0" + End
                         ChromosomeList[i].chromosomeBinar = ChromosomeBinarFinal
                     }
                 }
@@ -190,11 +182,11 @@ import java.util.*
                 ChromosomeList[i].ch = decimal
                 ChromosomeList2[i].ch = decimal
             }
-            NumOfIterations++
+            numOfIterations++
         }
-        println("\n | Liczba iteracji: $NumOfIterations | ")
+        println("\n | Liczba iteracji: $numOfIterations | ")
         //System.out.println("Największy fenotyp: "+FinalValue);
-        println(" | Największa funkcja przystosowania: " + Final / 6 + " | ")
+        println(" | Największa funkcja przystosowania: " + final / 6 + " | ")
         for (i in 0 until ChromosomesNumber) {
             val j = i + 1
             print(" | CH" + j + " = " + ChromosomeList[i].chromosomeBinar + " | ")
